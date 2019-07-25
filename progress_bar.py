ea5c6b85e6f2fd2c9b735ecdfc37c8082174a1b8
littleyk <kelanlittley@gmail.com>
2019-07-25 21:51:09 +0100

    Removes dev code. Changed to class to be used with 'with' statement

--- progress_bar.py
+++ progress_bar.py
@@ -1,24 +1,28 @@
-import time
 import shutil
 
 WIDTH = 50
 
-def prog_msg(done, total, msg=None):
+class ProgWithMsg(object):
 
-    if msg: 
-        termw = shutil.get_terminal_size((80, 20))[0]
-        clear = " " * (termw - 1 - len(msg))
-        print(msg + clear, flush=True)
+    def __init__(self, width=WIDTH):
+
+        self.width = width
+
+    def __enter__(self):
+
+        return self
     
-    prog = int(WIDTH * done / total) + 1
-    bar = "\r[{}{}] {:.02f}% {}/{}".format('=' * prog, ' ' * (WIDTH - prog), ((done+1)/total) * 100, done + 1, total)
-    print(bar, end="\r", flush=True)
+    def update(self, done, total, msg=None):
 
-r = 137
-for i in range(r):
+        if msg: 
+            termw = shutil.get_terminal_size((80, 20))[0]
+            clear = " " * (termw - 1 - len(msg))
+            print(msg + clear, flush=True)
+        
+        prog = int(WIDTH * done / total) + 1
+        bar = "\r[{}{}] {:.02f}% {}/{}".format('=' * prog, ' ' * (WIDTH - prog), ((done+1)/total) * 100, done + 1, total)
+        print(bar, end="\r", flush=True)
 
-    msg = "The value is " + str(i)
-    prog_msg(i, r, msg)
-    time.sleep(0.1)
-
-print()
+    def __exit__(self, exc_type, exc_value, traceback): 
+        
+        print()
